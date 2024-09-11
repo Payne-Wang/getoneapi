7704918c1069f8c8e10fb638738b07e22fced368
Payne-Wang <44337721+Payne-Wang@users.noreply.github.com>
2024-09-11 18:03:58 +0800

    Create getloginapi.r

---
+++ getloginapi.r
@@ -0,0 +1,55 @@
+library(httr)
+library(jsonlite)
+#你的oneapi站点，不要有斜杠
+url <- c('my_url')
+
+# 根据网页请求模拟登录
+login <- function(username, password,url=NULL) {
+  data <- list(
+    username = username,
+    password = password
+  )
+  last_url <- modify_url(url, path = "/api/user/login", query = list(turnstile = ""))
+  
+  resp <- POST(last_url, 
+               handle = handle(url),
+               body = data,
+               encode = "json",
+               timeout(3),
+               config(ssl_verifypeer = FALSE))
+  
+  if (status_code(resp) == 200) {
+    content <- content(resp, "parsed")
+    if (content$success) {
+      print(paste("登录成功：", url))
+    }
+  }
+}
+
+
+# 使用safely来包装login函数，防止报错使循环中断
+safe_login <- safely(login)
+
+# 对URL列表应用登录函数
+results <- map(url, ~safe_login("root", "123456", url = .))
+
+# 渠道函数
+channel <- function(page, page_size) {
+  last_url <- modify_url(url, path = "/api/channel/", 
+                         query = list(p = page, page_size = page_size, id_sort = "false"))
+  
+  resp <- GET(last_url, 
+              handle = session,
+              timeout(3),
+              config(ssl_verifypeer = FALSE))
+  
+  if (status_code(resp) == 200) {
+    content <- content(resp, "parsed")
+    if (content$success) {
+      print(content$data)
+    }
+  }
+}
+
+#登陆后查看渠道信息
+channel(1,10)
